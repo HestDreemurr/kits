3a559cf63d8b1f9377a6de494e489949c36b54c8
Hest <hestdatabase@gmail.com>
2024-09-25 09:15:40 -0300

    Google Auth

---
+++ script.js
@@ -0,0 +1,3 @@
+function enviar() {
+  window.location.href = "https://accounts.google.com/o/oauth2/auth?client_id=rede-social-teste-435721&redirect_uri=https://kets.netlify.app&response_type=code&scope=https://www.googleapis.com/auth/drive.file"
+}
