dbcde05761db74a7df1f81ebf107d533884ad6c1
Hest <hestdatabase@gmail.com>
2024-09-25 09:30:02 -0300

    Client ID

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function enviar() {
-  window.location.href = "https://accounts.google.com/o/oauth2/auth?client_id=rede-social-teste-435721&redirect_uri=https://kets.netlify.app&response_type=code&scope=https://www.googleapis.com/auth/drive.file"
+  window.location.href = "https://accounts.google.com/o/oauth2/auth?client_id=374824979446-he69p9l8abf4fdl3pkq5khmdc3sgrps3.apps.googleusercontent.com&redirect_uri=https://kets.netlify.app&response_type=code&scope=https://www.googleapis.com/auth/drive.file"
 }
